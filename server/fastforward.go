e2c43ed923cdb31c3143b902195a01194cef8da7
Jongmin Baek <jongmin@dropbox.com>
2018-10-01 13:37:17 -0700

    Add comment and improve readability

--- server/fastforward.go
+++ server/fastforward.go
@@ -65,13 +65,13 @@ func analyzeEditAndMapLine(source_lines, target_lines []string, source_lineno in
 	if len(source_lines) > SOURCE_CHUNK_MAX_CONTEXT {
 		// HAX(jongmin): Constraint the # of source lines we run on to avoid quadratic runtime.
 		new_start := max(source_lineno-(SOURCE_CHUNK_MAX_CONTEXT/2), 1)
-		new_end := new_start + SOURCE_CHUNK_MAX_CONTEXT - 1
+		new_end := new_start + SOURCE_CHUNK_MAX_CONTEXT // exclusive
 		if new_end > len(source_lines) {
-			new_start = max(len(source_lines)-SOURCE_CHUNK_MAX_CONTEXT+1, 1)
-			new_end = len(source_lines)
+			new_end = len(source_lines) + 1
+			new_start = max(new_end - SOURCE_CHUNK_MAX_CONTEXT, 1)
 		}
 		new_source_lineno := source_lineno - new_start + 1
-		return analyzeEditAndMapLine(source_lines[new_start-1:new_end], target_lines, new_source_lineno)
+		return analyzeEditAndMapLine(source_lines[new_start-1:new_end-1], target_lines, new_source_lineno)
 	}
 	source_chars := strings.Join(source_lines, "\n")
 	target_chars := strings.Join(target_lines, "\n")
